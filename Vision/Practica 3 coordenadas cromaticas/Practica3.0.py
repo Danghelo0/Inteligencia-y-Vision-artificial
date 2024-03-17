593b92f424019757a8c9b4eb217e15bcb30382ef
Danghelo0 <danghelov356@gmail.com>
2024-03-16 20:29:16 -0600

    .

--- Vision/Practica 3 coordenadas cromaticas/Practica3.0.py
+++ Vision/Practica 3 coordenadas cromaticas/Practica3.0.py
@@ -7,18 +7,17 @@ Created on Thu Mar  7 00:17:22 2024
 
 import cv2
 import numpy as np
-
 imagen=cv2.imread("lambo.jpg")
 m,n,c=imagen.shape
-imagenc=imagen.copy()
-imagenc=imagenc.astype(np.float32)
 imagen=imagen.astype(np.float32)
+imagenc=imagen.astype(np.float32)
 
 for x in range (m):
     for y in range(n):
         imagenc[x,y,0]=imagen[x,y,0]/(imagen[x,y,0]+imagen[x,y,1]+imagen[x,y,2])
         imagenc[x,y,1]=imagen[x,y,1]/(imagen[x,y,0]+imagen[x,y,1]+imagen[x,y,2])
         imagenc[x,y,2]=imagen[x,y,2]/(imagen[x,y,0]+imagen[x,y,1]+imagen[x,y,2])
+imagenc = (imagenc * 255).astype(np.uint8)
         
 cv2.imshow("lambo cromatico",imagenc)
 cv2.imwrite('lambo cromatico.jpg',imagenc)
